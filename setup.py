246141760ea0e182d2e63daeaaed9e6f094b27e2
Chen Liu <chen.liu.cl2482@yale.edu>
2023-02-19 22:15:02 -0500

    update readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='gan-evaluator',
-    version='1.12',
+    version='1.13',
     license='MIT',
     author='Chen Liu',
     author_email='chen.liu.cl2482@yale.edu',
